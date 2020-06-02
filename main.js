6b196697bb06a9455ed1f8640b7bdfcd177c23d9
IannGeorges <ianngeorges@gmail.com>
2020-06-01 19:20:17 -0500

    Added Javascript function 'addTodo' to add Todo List, still need add complete or discard option to list items

--- main.js
+++ main.js
@@ -14,7 +14,25 @@ function addTodo(event){
    // Prevent form from subtmitting 
     event.preventDefault();
     // Todo Div
-    const todoDiv = document.createElement('div');
+    const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
-    // Create 
+    // Create Li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //CHECK MARK BUTTON
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class = "fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //CHECK TASH BUTTON
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class = "fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    //APPEND TO LIST
+    todoList.appendChild(todoDiv);
+    //CLEAR todo Input Value
+    todoInput.value = "";
 }
